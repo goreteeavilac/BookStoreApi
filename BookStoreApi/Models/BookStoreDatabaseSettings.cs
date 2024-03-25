26a05601a3bdd2e5d8fb89b18e5a20cc058be62a
GORETEE VIRIDIANA AVILA CIENEGA <goretee.avilac@gto.cecyteg.edu.mx>
2024-03-24 23:29:39 -0600

    Agregar archivos de proyecto.

---
+++ BookStoreApi/Models/BookStoreDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStoreApi.Models
+{
+    public class BookStoreDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string BooksConnectionName { get; set; } = null!;
+    }
+}
